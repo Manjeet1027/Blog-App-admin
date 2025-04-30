9e6ccc5b20b5d5c5d9be4fac1fa7c4ac13e6b4be
Manjeet <manjeet@extensioncrm.com>
2025-04-30 20:06:01 +0530

    .

--- client/src/components/BlogCard.js
+++ client/src/components/BlogCard.js
@@ -37,7 +37,7 @@ export default function BlogCard({
       if (data?.success) {
         alert("Blog Deleted");
         window.location.reload();
-        navigate("/blogs")
+        navigate("/")
       }
     } catch (error) {
       console.log(error);
